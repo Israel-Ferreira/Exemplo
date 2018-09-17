66b91467d60c48ccd13e5f9d91b166be428cdf90
Israel Ferreira de Souza <israel.souza5@fatec.sp.gov.br>
2018-09-17 00:47:54 -0300

    Adicionando Interação no select

--- js/main.js
+++ js/main.js
@@ -1,11 +1,41 @@
 let form = document.getElementById("form");
-
+let tckSel = document.getElementById("ticket");
 
 const accessElemById = elId => document.getElementById(elId).value;
 
-const formIsEmpty = arrFields => {
-    return arrFields.some(field => field === "");
+const formIsEmpty = arrF => arrF.some(field => field === "");
+
+const checkIsEmpty = arrCheck => arrCheck.every(cb => !cb.checked);
+
+const accessInputs = arr => arr.map(id => accessElemById(id));
+
+const priceSel = val => {
+    if(val === "Passeio Normal"){
+        return 250;
+    }else if(val === "Passeio Panorâmico"){
+        return 350;
+    }else if(val === "Expresso da Meia Noite"){
+        return 500;
+    }else{
+        return 0;
+    }
 }
+
+
+
+tckSel.addEventListener('change',event => {
+    let sel = event.target.value;
+    priceChanged(sel)
+});
+
+
+
+
+const priceChanged = (eVal) => {
+    let price = priceSel(eVal);
+    document.getElementById("price").innerHTML = price.toFixed(2).toString();
+}
+
 
 
 const formError = () => {
@@ -15,15 +45,16 @@ const formError = () => {
     errorMsg.textContent = msg;
 }
 
+tckSel.addEventListener('change',event => {
+    let sel = event.target.value;
+    priceChanged(sel)
+});
+
 
 form.addEventListener('submit', event => {
+    let ids = ["first-name","last-name","ticket","amount"];
 
-    let nome = accessElemById('first-name');
-    let sobrenome = accessElemById('last-name');
-    let ticket = accessElemById("ticket");
-    let qtde = accessElemById("amount");
-
-    let campos = [nome,sobrenome,ticket,qtde];
+    let campos = accessInputs(ids);
 
     if(formIsEmpty(campos)){
         event.preventDefault();
