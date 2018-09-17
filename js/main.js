c5b5ce7ab653702be004caa16395fc51819015b7
Israel Ferreira de Souza <israel.souza5@fatec.sp.gov.br>
2018-09-17 03:05:07 -0300

    Por hoje, é só

--- js/main.js
+++ js/main.js
@@ -1,54 +1,29 @@
 let form = document.getElementById("form");
-let tckSel = document.getElementById("ticket");
 
 const accessElemById = elId => document.getElementById(elId).value;
 
 const formIsEmpty = arrF => arrF.some(field => field === "");
 
-const checkIsEmpty = arrCheck => arrCheck.every(cb => !cb.checked);
-
 const accessInputs = arr => arr.map(id => accessElemById(id));
 
-const priceSel = val => {
-    if(val === "Passeio Normal"){
-        return 250;
-    }else if(val === "Passeio Panorâmico"){
-        return 350;
-    }else if(val === "Expresso da Meia Noite"){
+
+const formError = () => {
+    let errorMsg = document.getElementById("err-msg");
+    errorMsg.style.display = "block";
+    errorMsg.textContent = "Campos Vazios!!";
+}
+
+const priceTrip =  opt => {
+    if(opt === "Expresso da Meia Noite"){
         return 500;
+    }else if(opt === "Passeio Panorâmico"){
+        return 350;
+    }else if(opt === "Passeio Normal"){
+        return 250;
     }else{
         return 0;
     }
 }
-
-
-
-tckSel.addEventListener('change',event => {
-    let sel = event.target.value;
-    priceChanged(sel)
-});
-
-
-
-
-const priceChanged = (eVal) => {
-    let price = priceSel(eVal);
-    document.getElementById("price").innerHTML = price.toFixed(2).toString();
-}
-
-
-
-const formError = () => {
-    let msg = "Campos Vazios!!";
-    let errorMsg = document.getElementById("err-msg");
-    errorMsg.style.display = "block";
-    errorMsg.textContent = msg;
-}
-
-tckSel.addEventListener('change',event => {
-    let sel = event.target.value;
-    priceChanged(sel)
-});
 
 
 form.addEventListener('submit', event => {
