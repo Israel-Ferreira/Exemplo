bcf59d44382f1219b43b9e3b75bd74af58cd3d24
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2018-09-20 03:16:06 -0300

    Adicionando as alterações

--- ler_dados.php
+++ ler_dados.php
@@ -13,42 +13,59 @@
     </header>
     <?php
         date_default_timezone_set("America/Sao_Paulo");
+
         function verifica_campo($campo){
             return isset($_POST[$campo]) ? true : false;
         }
 
-        if(verifica_campo('first-name') && verifica_campo('last-name')){
-            $nome =  $_POST['first-name'];
-            $sobrenome = $_POST['last-name'];
+        function valorCampo($campo){
+            if(verifica_campo($campo)){
+                return $_POST[$campo];
+            }
         }
 
-        if(verifica_campo('ticket')){
-            $tipo_bilhete = $_POST['ticket'];
+        function precoOpcional($arrOpt){
+            $precoOp = 0;
+            
+            if(gettype($arrOpt) === null){
+                return 0;
+            }else{    
+                foreach($arrOpt as $opt){
+                    if($opt === "doce de leite"){
+                        $precoOp += 15.00;
+                    }else if($opt === "queijo"){
+                        $precoOp += 25.00;
+                    }else{
+                        $precoOp += 14.00;
+                    }
+                }
+
+                return $precoOp;
+            }
         }
 
-        if(verifica_campo('qtde_bilhetes')){
-            $qtde = $_POST['qtde_bilhetes'];
-        }
-
-        if(verifica_campo('data-de-partida')){
-            $data_partida = $_POST['data-de-partida'];
-        }
-
-        if(verifica_campo('seguro')){
-            $seguro = $_POST['seguro'];
-        }
-
-        function get_volume($preco,$qtde){
-            return $preco * $qtde;
-        }
-
-        function get_valor($passeio, $qtde){
-            if($passeio === "Passeio Normal"){
-                return get_volume(250,$qtde);
-            }else if($passeio === "Passeio Panorâmico"){
-                return get_volume(350,$qtde);
+        function precoFinal($passeio,$qtde,$precoOp,$tipo_vagao){
+            $preco = valorPasseio($passeio);
+            if($tipo_vagao == "Vagao Economico"){
+                return get_preco($preco,$qtde,$precoOp);
+            }else if($tipo_vagao === "Vagão Executivo"){
+                return get_preco($preco,$qtde,$precoOp,200);
             }else{
-                return get_volume(500,$qtde);
+                return get_preco($preco,$qtde,$precoOp,350);
+            }
+        }
+
+        function get_preco($preco,$qtde,$opc,$preco_vagao = 0){
+            return $preco * $qtde + $opc + $preco_vagao;
+        }
+
+        function valorPasseio($passeio){
+            if($passeio === "Passeio Normal"){
+                return 250;
+            }else if($passeio === "Passeio Panorâmico"){
+                return 350;
+            }else{
+                return 500;
             }
         }
 
@@ -62,21 +79,35 @@
             fclose($fp);
         }
 
-        
+        $nome =  valorCampo('first-name');
+        $sobrenome = valorCampo('last-name');
+
+        $opcionais = valorCampo('optional');
+        $valor_opcional = precoOpcional($opcionais);
+
+        echo "<h2>$opcionais</h2>";
+
+
+        $tipo_bilhete = valorCampo('ticket');
+        $qtde = valorCampo('qtde_bilhetes');
+
+        $data_partida = valorCampo('data-de-partida');
+        $tipo_vagao = valorCampo('vagao');
+
         $data = date("d/m/Y H:i:s");
-        $preco =  get_valor($tipo_bilhete,$qtde);
+        $preco =  precoFinal($tipo_bilhete,$qtde,$valor_opcional,$tipo_vagao);
 
         $data_msg = "Data da compra: $data";
-        $data_prt = "Data de Partida: $data_partida";
+        $preco = number_format($preco,2);
+        $preco_msg = "R$ $preco";
 
-        $dados = array($nome, $tipo_bilhete,$qtde,$preco,$data_msg,$data_prt);
+        $dados = array($nome,$tipo_bilhete,$qtde,$preco_msg,$data_partida,$data_msg);
+
         emitir_passagem($dados);
-
 
         foreach ($dados as $dado) {
             echo "<h2>$dado</h2>";
         }
-        
     ?>
     
 </body>
